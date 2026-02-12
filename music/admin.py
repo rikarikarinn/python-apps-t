af6dd88fdc3cbd7d080bf821dd65962c16d07b2b
rikarikarinn <nazuki_shibata@icloud.com>
2026-02-12 09:38:08 +0900

    .

--- music/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Genre, Artist, Track
-
-admin.site.register(Genre)
-admin.site.register(Artist)
-admin.site.register(Track)
--- music/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Track
-import random
-
-def recommend(request):
-    tracks = list(Track.objects.all())
-    track = random.choice(tracks) if tracks else None
-    return render(request, 'music/recommend.html', {'track': track})
